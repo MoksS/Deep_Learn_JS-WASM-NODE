eaed67d23fbc2dbf39faf8e8b24d71f0bc15d705
maksim <skarobogyodko@gmail.com>
2020-01-26 22:37:51 +0300

    modified:   Sessions/client.js 	modified:   Sessions/server.js 	modified:   Sessions/session.js 	modified:   Sessions/storage.js 	modified:   http2_node_test/http2Client.js 	modified:   http2_node_test/http2tTest.js

--- http2_node_test/http2Client.js
+++ http2_node_test/http2Client.js
@@ -1,13 +1,15 @@
-const http2 = require('http2');
-const client = http2.connect('http://localhost:8000');
-const req = client.request({ ':method': 'GET', ':path': '/' });
+const http2 = require("http2");
+
+const client = http2.connect("http://localhost:8000");
+const req = client.request({ ":method": "GET", ":path": "/" });
 const data = [];
-req.on('response', (responseHeaders) => {
+req.on("response", responseHeaders => {
   console.log(responseHeaders);
 });
-req.on('data', (chunk) => {
+req.on("data", chunk => {
   data.push(chunk);
 });
-req.on('end', () =>{
-    console.log(data.toString('utf8'));
-    client.destroy()});
+req.on("end", () => {
+  console.log(data.toString("utf8"));
+  client.destroy();
+});
--- http2_node_test/http2tTest.js
+++ http2_node_test/http2tTest.js
@@ -1,10 +1,11 @@
-const http2 = require('http2');
+const http2 = require("http2");
+
 const server = http2.createServer();
-server.on('stream', (stream, requestHeaders) => {
-  console.log(requestHeaders, '\n', stream);
-  stream.respond({'status': 200, 'content-type': 'text/plain' });
-  stream.write('hello world');
-  stream.write('hello епта');
-  stream.end('\n яяяязь');
+server.on("stream", (stream, requestHeaders) => {
+  console.log(requestHeaders, "\n", stream);
+  stream.respond({ status: 200, "content-type": "text/plain" });
+  stream.write("hello world");
+  stream.write("hello епта");
+  stream.end("\n яяяязь");
 });
 server.listen(8000);
