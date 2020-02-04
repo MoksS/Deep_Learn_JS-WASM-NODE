f7bfee04f22648bee1b494637f341bbd7020bdaa
maksim <skarobogyodko@gmail.com>
2020-02-04 20:00:56 +0300

    test TypedArrays

---
+++ TypedArrays/6-reduce.js
@@ -0,0 +1,11 @@
+const arr8 = new Int8Array(10);
+
+for (let i = 0; i < 10; i++) {
+  arr8[i] = i;
+}
+
+const reduced = arr8.reduce((acc, el) => acc + el);
+console.dir({
+  reduced,
+  type: typeof reduced
+});
